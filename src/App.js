248c66e30c679bf15a8b8012d69ccd1683f87e59
Alexander Sosov <nookat.develop@gmail.com>
2022-07-28 16:34:14 +0300

    feat: added the header

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      Welcome to hooks
-    </div>
-  );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import Routes from 'routes'
+import TopBar from 'components/topBar'
+
+const App = () => (
+  <div>
+    <Router>
+      <TopBar/>
+      <Routes/>
+    </Router>
+  </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('root'))
