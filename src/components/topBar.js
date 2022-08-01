13fa1d63a0ed7792a71dd9af71bdfd77f6de8540
Alexander Sosov <nookat.develop@gmail.com>
2022-08-01 12:17:55 +0300

    feat: updated the menu for the authorized user

--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,6 +1,11 @@
+import {useContext} from 'react'
 import {Link, NavLink} from 'react-router-dom'
+import {CurrentUserContext} from 'contexts/currentUser'
 
 const TopBar = () => {
+  const [currentUserState] = useContext(CurrentUserContext)
+  console.log('currentUserState', currentUserState)
+
   return (
     <nav className="navbar navbar-light">
       <div className="container">
@@ -13,16 +18,40 @@ const TopBar = () => {
               Home
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/login" className="nav-link">
-              Sign in
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/register" className="nav-link">
-              Sign up
-            </NavLink>
-          </li>
+          {
+            currentUserState.isLoggedIn === false && (
+              <>
+                <li className="nav-item">
+                  <NavLink to="/login" className="nav-link">
+                    Sign in
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" className="nav-link">
+                    Sign up
+                  </NavLink>
+                </li>
+              </>
+            )
+          }
+          {
+            currentUserState.isLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <NavLink to="/articles/new" className="nav-link">
+                    <i className="ion-compose"></i>
+                    &nbsp; New Post
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to={`/profiles/${currentUserState.currentUser.username}`} className="nav-link">
+                    <img className="user-pic" src={currentUserState.currentUser.image} alt=""/>
+                    &nbsp; {currentUserState.currentUser.username}
+                  </NavLink>
+                </li>
+              </>
+            )
+          }
         </ul>
       </div>
     </nav>
