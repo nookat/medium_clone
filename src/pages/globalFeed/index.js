cd5493876882e120c1c644a9939d4f453aa75064
Alexander Sosov <nookat.develop@gmail.com>
2022-08-01 15:01:02 +0300

    feat: added the feed component

--- src/pages/globalFeed/index.js
+++ src/pages/globalFeed/index.js
@@ -1,5 +1,38 @@
+import {useEffect} from 'react'
+
+import Feed from 'components/feed'
+import useFetch from 'hooks/useFetch'
+
 const GlobalFeed = () => {
-  return <div>GlobalFeed</div>
+  const apiUrl = '/articles?limit10&offset=0'
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl)
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium clone</h1>
+          <p>A place to share knowledge</p>
+        </div>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>Loading...</div>}
+            {error && <div>Some error happened</div>}
+            {!isLoading && response && (<Feed articles={response.articles}/>)}
+          </div>
+          <div className="col-md-3">
+            Popular tags
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default GlobalFeed
