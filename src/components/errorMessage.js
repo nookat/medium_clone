d30f7169f79a4137e64257fc0a5b77fd72472f78
Alexander Sosov <nookat.develop@gmail.com>
2022-08-02 11:08:21 +0300

    feat: added pagination and tags

---
+++ src/components/errorMessage.js
@@ -0,0 +1,5 @@
+const ErrorMessage = () => {
+  return <div>Something went wrong</div>
+}
+
+export default ErrorMessage
--- src/pages/globalFeed/index.js
+++ src/pages/globalFeed/index.js
@@ -1,15 +1,27 @@
 import {useEffect} from 'react'
+import {stringify} from 'query-string'
 
+import Loading from 'components/loading'
+import ErrorMessage from 'components/errorMessage'
 import Feed from 'components/feed'
+import Pagination from 'components/pagination'
+import PopularTags from 'components/popularTags'
 import useFetch from 'hooks/useFetch'
+import {getPaginator, limit} from 'utils'
 
-const GlobalFeed = () => {
-  const apiUrl = '/articles?limit10&offset=0'
+const GlobalFeed = ({ location, match }) => {
+  const { offset, currentPage } = getPaginator(location.search)
+  const stringifiedParams = stringify({
+    limit,
+    offset
+  })
+  const url = match.url
+  const apiUrl = `/articles?${stringifiedParams}`
   const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl)
 
   useEffect(() => {
     doFetch()
-  }, [doFetch])
+  }, [doFetch, currentPage])
 
   return (
     <div className="home-page">
@@ -22,12 +34,23 @@ const GlobalFeed = () => {
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
-            {isLoading && <div>Loading...</div>}
-            {error && <div>Some error happened</div>}
-            {!isLoading && response && (<Feed articles={response.articles}/>)}
+            {isLoading && <Loading/>}
+            {error && <ErrorMessage/>}
+            {
+              !isLoading && response && (
+                <>
+                  <Feed articles={response.articles}/>
+                  <Pagination
+                    total={response.articlesCount}
+                    limit={limit}
+                    url={url}
+                    currentPage={currentPage}/>
+                </>
+              )
+            }
           </div>
           <div className="col-md-3">
-            Popular tags
+            <PopularTags/>
           </div>
         </div>
       </div>
