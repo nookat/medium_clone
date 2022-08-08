8db86d7e389211ecca610ecffa4c9c2a4a6ed2c0
Alexander Sosov <nookat.develop@gmail.com>
2022-08-08 14:51:03 +0300

    feat: created feed page

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,5 +1,57 @@
+import {useEffect} from 'react'
+import {Link, useParams} from 'react-router-dom'
+
+import Loading from 'components/loading'
+import ErrorMessage from 'components/errorMessage'
+import TagList from 'components/tagList'
+
+import useFetch from 'hooks/useFetch'
+
 const Article = () => {
-  return <div>Article</div>
+  const { slug } = useParams()
+  const apiUrl = `/articles/${slug}`
+  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl)
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+  return (
+    <div className="article-page">
+      <div className="banner">
+        {!isLoading && response && (
+          <div className="container">
+            <h1>{response.article.title}</h1>
+            <div className="article-meta">
+              <Link to={`/profiles/${response.article.author.username}`}>
+                <img src={response.article.author.image} alt=""/>
+              </Link>
+              <div className="info">
+                <Link to={`/profiles/${response.article.author.username}`}>
+                  {response.article.author.username}
+                </Link>
+                <span className="date">{response.article.createdAt}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="container page">
+        {isLoading && <Loading/>}
+        {error && <ErrorMessage/>}
+        {!isLoading && response && (
+          <div className="row article-content">
+            <div className="col-xs-12">
+              <div>
+                <p>{response.article.body}</p>
+              </div>
+              <TagList tags={response.article.tagList}/>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Article
